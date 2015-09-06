c1b77cf00bc295e93956ed1fc3ef65a6dba9a3a7
royschuyler <royschuyler@yahoo.com>
2015-09-06 10:46:02 -0500

    breakdown of the drink cost appending to html

--- README.md
+++ README.md
@@ -3,5 +3,6 @@ Drinks is a simple program I made to calculate the cost of menu drinks at bars/r
 Its made for bar managers/owners to input the
   - cost of bulk ingredients
   - amount used per drink
+
 Then calculate the cost for each drink made.
 Please try it out!
--- script.js
+++ script.js
@@ -6,32 +6,44 @@ function Item(name, price, ozPer, ozUsed) {
   this.cost = (this.price / this.ozPer) * this.ozUsed;
 }
 
-var btn = $('.btn')
+var btn = $('.btn');
+var breakdownBtn = $('.breakdownBtn');
 
 btn.on('click', function() {
-  var nameA = $('.nameA').val();
-  var priceA = $('.a').val();
-  var ozPerA = $('.b').val();
-  var ozUsedA = $('.c').val();
+  var drinkName = $('.drinkName').val();
+  var nameA     = $('.nameA').val();
+  var priceA    = $('.a').val();
+  var ozPerA    = $('.b').val();
+  var ozUsedA   = $('.c').val();
 
-  var nameB = $('.nameB').val();
-  var priceB = $('.d').val();
-  var ozPerB = $('.e').val();
-  var ozUsedB = $('.f').val();
+  var nameB     = $('.nameB').val();
+  var priceB    = $('.d').val();
+  var ozPerB    = $('.e').val();
+  var ozUsedB   = $('.f').val();
 
-  var nameC = $('.nameB').val();
-  var priceC = $('.d').val();
-  var ozPerC = $('.e').val();
-  var ozUsedC = $('.f').val();
+  var nameC     = $('.nameC').val();
+  var priceC    = $('.g').val();
+  var ozPerC    = $('.h').val();
+  var ozUsedC   = $('.i').val();
 
   var ingredient1 = new Item(nameA, priceA, ozPerA, ozUsedA);
   var ingredient2 = new Item(nameB, priceB, ozPerB, ozUsedB);
   var ingredient3 = new Item(nameC, priceC, ozPerC, ozUsedC);
   var cost = ingredient1.cost + ingredient2.cost + ingredient3.cost;
 
-  var string = "It costs $" + cost + " to make this drink."
-  var target = $('.target')
+  var target = $('.target');
+  var breakdownTarget = $('.breakdownTarget');
+  var string = "It costs $" + cost + " to make this " + drinkName + ".";
+  var breakdownString =
+    '<li>The ' + ingredient1.name + ' cost is ' + ingredient1.cost + '.</li>' +
+    '<li>The ' + ingredient2.name + ' cost is ' + ingredient2.cost + '.</li>' +
+    '<li>The ' + ingredient3.name + ' cost is ' + ingredient3.cost + '.</li>';
+
+
+
   target.append(string);
-  console.log(cost);
+  breakdownTarget.append(breakdownString);
 
 });
+
+
